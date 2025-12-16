2374101b22162254b407e7e6e939d704c69cb0b7
xenazelda <zdara@terpmail.umd.edu>
2025-12-16 15:38:19 -0500

    Did JS code for Surprise. Need to fix Ingredient JS code.

--- project/ingredient.js
+++ project/ingredient.js
@@ -3,7 +3,9 @@ async function populateIngredients() {
     const ingredients = await fetch("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").then(
         (result) => result.json()
     );
-    const datalist = document.getElementById("ingredient");
+    const datalist1 = document.getElementById("ingredient1");
+    const datalist2 = document.getElementById("ingredient2");
+    const datalist3 = document.getElementById("ingredient3");
 
     ingredients.drinks.forEach(ingredient => {
         console.log(`Drink: ${JSON.stringify(ingredient)}`);
@@ -11,7 +13,10 @@ async function populateIngredients() {
         const option = document.createElement("option");
         option.value = ingredient.strIngredient1;
         option.innerHTML = ingredient.strIngredient1;
-        datalist.appendChild(option);
+
+        datalist1.appendChild(option.cloneNode(true));
+        datalist2.appendChild(option.cloneNode(true));
+        datalist3.appendChild(option.cloneNode(true));
     });
 }
 
---
+++ project/surprise.js
@@ -0,0 +1,43 @@
+function surpriseDrink() {
+    const cocktailName = document.getElementById("cocktail-name");
+    const cocktailImg = document.getElementById("cocktail-img");
+    const cocktailInstructions = document.getElementById("p-1");
+    const cocktailIngredients = document.getElementById("p-2");
+
+    // generating a random cocktail
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
+    .then((result) => result.json())
+    .then((data) => {
+        const drink = data.drinks[0];
+
+        // update name and image
+        cocktailName.textContent = drink.strDrink;
+        cocktailImg.src = drink.strDrinkThumb;
+
+        // update instructions
+        cocktailInstructions.textContent = drink.strInstructions;
+        
+        // update ingredients list
+        let ingredientList = "";
+
+        // loop goes through all 15 ingredients and measures in the api
+        for(let i = 1; i <= 15; i++) {
+            const ingredient = drink[`strIngredient${i}`];
+            const measure = drink[`strMeasure${i}`];
+
+            // checks if there are both ingredients and measurements from the api
+            if(ingredient) {
+                if(measure) {
+                    ingredientList += measure + " " + ingredient + ", ";
+                }
+                else {
+                    ingredientList += ingredient + ", ";
+                }
+            }
+
+            cocktailIngredients.textContent = ingredientList.trim();
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", surpriseDrink);
